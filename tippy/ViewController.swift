c45f45008f5786e68dcc56498efc2dffa84d6460
Xiuya Yao <xiuyayao@xiuyayao.dhcp.thefacebook.com>
2017-06-20 16:16:31 -0700

    20% tip calculator

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        // print("Hello")
+        view.endEditing(true) // Dismiss keyboard
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billField.text!) ?? 0
+        // 0 is default value if invalid input
+        let tip = bill * 0.2
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
